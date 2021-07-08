40a7d09e5af563ca12432a97a657b71916a5d304
dglass710 <66642674+dglass710@users.noreply.github.com>
2021-07-08 00:52:20 -0500

    Update cliDG.sh

--- cliDG.sh
+++ cliDG.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # cd /path/to/directory/containing/these/files # Uncomment to use this as a command (as well as cp cliDG.sh commandname; chmod +x commandname; mv commandname /usr/local/bin) 
-rm test.* > /dev/null 2>&1 
+rm diagram.* > /dev/null 2>&1 
 py cliDG.py $@&& pdflatex diagram.tex&& open diagram.pdf
